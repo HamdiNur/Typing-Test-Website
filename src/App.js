af0a6ece50a6cfbcd6c1e7685bcbce110d072806
HamdiNur <hamdoonur26@gmail.com>
2024-07-30 06:59:58 +0300

    upload all

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import TypingBox from "./components/TypingBox";
+import { GlobalStyles } from "./styles/Global";
+
+function App() {
+  return (
+    <div className="canvas">
+      <GlobalStyles/>
+        <div>Header</div>
+         
+        <TypingBox/>
+        <div>Footer</div>
+
+    </div>
+  );
+}
+
+export default App;
